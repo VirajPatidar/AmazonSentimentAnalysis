f6c0142e2011cad0f8fe80bbdc77dd75fdb8f557
shadyskies <mehta.naman328@gmail.com>
2022-04-04 21:37:03 +0530

    added predict function to api endpoint

--- backend/sentiment_analysis_server.py
+++ backend/sentiment_analysis_server.py
@@ -2,8 +2,24 @@ from tokenize import String
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+
 
 app = FastAPI()
+# loading model and tokenizer
+model = load_model('sentiment_analysis')
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tokenizer_from_json(data)
+max_length = 100
+padding_type="post"
+trunc_type="post"
+
 
 class Sentiment(BaseModel):
     message: str
@@ -11,9 +27,22 @@ class Sentiment(BaseModel):
 
 @app.post("/api/get_sentiment/")
 async def return_sentiment(sentiment: Sentiment):
+    # text message should be less than 100 characters
+    if len(sentiment.message) > 100:
+        return {
+            "status": "Forbidden",
+            "data": "Sentence should be less than 100 characters" 
+        }
+    test_this = [sentiment.message]
+    print(f"this is the test input: {test_this}")
+    sequences = tokenizer.texts_to_sequences(test_this)
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    prob=model.predict(padded)
+    print(prob[0][0])
     return {
-        "status": "OK",
-        "data": sentiment
+        "status": "OK", 
+        "probability": float(prob[0][0]) 
     }
 
 @app.get('/ping/')
